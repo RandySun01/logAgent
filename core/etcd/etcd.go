3acc249d10c44ccf7d2763eebd0ceaecdca43791
RandySun01 <2214644978@qq.com>
2021-09-12 20:22:55 +0800

    添加etcd连接

--- core/etcd/etcd.go
+++ core/etcd/etcd.go
@@ -1,5 +1,59 @@
 package etcd
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"go.etcd.io/etcd/clientv3"
+	"time"
+)
+
 /*
 @author RandySun
 @create 2021-09-12-14:22
 */
+
+// etcd相关操作
+type collectEntry struct {
+	Path  string `json:"path"`
+	Topic string `json:"topic"`
+}
+var (
+	client * clientv3.Client
+)
+func Init(address []string) (err error) {
+	client, err = clientv3.New(clientv3.Config{
+		Endpoints:   address,
+		DialTimeout: time.Second * 5,
+	})
+	if err != nil {
+		fmt.Printf("connect to etcd failed, err:%v", err)
+		return
+	}
+	return
+}
+
+// 拉取日志收集配置项的函数
+func GetConf(key string) (collectEntryList []collectEntry, err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
+	defer cancel()
+	resp, err := client.Get(ctx, key)
+	if err != nil {
+		logrus.Errorf("get conf from etcd by key:%s failed ,err:%v", key, err)
+		return
+	}
+	if len(resp.Kvs) == 0 {
+		logrus.Warningf("get len:0 conf from etcd by key:%s", key)
+		return
+	}
+	ret := resp.Kvs[0]
+	// ret.Value // json格式字符串
+	fmt.Println("json格式字符串", string(ret.Value))
+	err = json.Unmarshal(ret.Value, &collectEntryList)
+	if err != nil {
+		logrus.Errorf("json unmarshal failed, err:%v", err)
+		return
+	}
+	return
+}

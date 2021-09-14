980c1c001541e10bee79e1cdbf9161e49cf09c01
RandySun01 <2214644978@qq.com>
2021-09-14 22:46:45 +0800

    管理日志收集的任务

--- core/tailfile/tailfLog.go
+++ core/tailfile/tailfLog.go
@@ -1,10 +1,10 @@
 package tailfile
 
 import (
+	"context"
 	"github.com/Shopify/sarama"
 	"github.com/hpcloud/tail"
 	"github.com/sirupsen/logrus"
-	"logAgent/common"
 	"logAgent/core/kafka"
 	"strings"
 	"time"
@@ -14,89 +14,79 @@ import (
 @author RandySun
 @create 2021-09-11-21:24
 */
-var (
-	TailObj *tail.Tail
-)
 
 // tail相关
 
 type tailTask struct { // 多个日志文件对象
-	path string
-	topic string
+	path    string
+	topic   string
 	tailObj *tail.Tail
+	ctx     context.Context // 为每一个tailTask任务创建一个上下文管理
+	cancel  context.CancelFunc
 }
 
 // 根据topic和path构造一个tailtask对象
-func newTailTask(path, topic string)  (tt *tailTask){
+func newTailTask(path, topic string) (tt *tailTask) {
+	// 添加上下文管理,控制tailTask任务
+	ctx, cancel := context.WithCancel(context.Background())
 	// 构造函数
 	tt = &tailTask{
-		path: path,
-		topic: topic,
+		path:   path,
+		topic:  topic,
+		ctx:    ctx,
+		cancel: cancel,
 	}
 	return
 }
 
 // 使用tail包打开日志文件准备读取日志
-func (t *tailTask) Init() (err error){
+func (t *tailTask) Init() (err error) {
 	// 初始划tail对象
 	config := tail.Config{
-		ReOpen:    true, // 打开文件
-		Follow:    true, // 文件切割自动重新打开
+		ReOpen:    true,                                 // 打开文件
+		Follow:    true,                                 // 文件切割自动重新打开
 		Location:  &tail.SeekInfo{Offset: 0, Whence: 2}, // Location读取文件的位置, Whence更加系统选择参数
-		MustExist: false, // 允许日志文件不存在
-		Poll:      true, // 轮询
+		MustExist: false,                                // 允许日志文件不存在
+		Poll:      true,                                 // 轮询
 	}
 
 	t.tailObj, err = tail.TailFile(t.path, config)
 	return
 }
+
 // 实际读取日志往kafka里面发送数据的方法
-func (t *tailTask) run(){
+func (t *tailTask) run() {
 	// logfile --> TailObj --> log --> Client --> kafka
 	logrus.Infof("collect for path: %s is running....", t.path)
-	for  {
-		//循环读取数据
-		line, ok := <- t.tailObj.Lines // chan tail.Line
-		if !ok{
-			logrus.Warn("tail file close reopen, path:%s\n", t.path)
-			time.Sleep(time.Second) // 读取出错等一秒
-			continue
-		}
+	for {
+		// 监听停止任务
+		select {
+		case <-t.ctx.Done():
+			logrus.Infof(" path %s is stop", t.path)
+			return
+		case line, ok := <-t.tailObj.Lines:
+			//循环读取数据
+			// chan tail.Line
+			if !ok {
+				logrus.Warn("tail file close reopen, path:%s\n", t.path)
+				time.Sleep(time.Second) // 读取出错等一秒
+				continue
+			}
 
-		// 如果是空行就略过
-		//fmt.Printf("%#v\n", line.Text)
-		if len(strings.Trim(line.Text,"\r")) == 0 {
-			logrus.Info("出现空行拉,直接跳过...")
-			continue
+			// 如果是空行就略过
+			//fmt.Printf("%#v\n", line.Text)
+			if len(strings.Trim(line.Text, "\r")) == 0 {
+				logrus.Info("出现空行拉,直接跳过...")
+				continue
+			}
+			// 利用通道将同步的代码改为异步的
+			// 把读出来的一行日志包装秤kafka里面的msg类型
+			msg := &sarama.ProducerMessage{}
+			msg.Topic = t.topic // 每一个tailObj自己的topic
+			msg.Value = sarama.StringEncoder(line.Text)
+			// 将消息发送到通道中
+			kafka.ToMsgChan(msg)
 		}
-		// 利用通道将同步的代码改为异步的
-		// 把读出来的一行日志包装秤kafka里面的msg类型
-		msg:=&sarama.ProducerMessage{}
-		msg.Topic = t.topic // 每一个tailObj自己的topic
-		msg.Value = sarama.StringEncoder(line.Text)
-		// 将消息发送到通道中
-		kafka.ToMsgChan(msg)
 
 	}
-}
-
-// 初始化tailTask,为每一个日志文件构造一个单独的tailTask
-func  Init(allEtcdConf []common.CollectEntry) (err error) {
-	// allEtcdConf里面的内容: [{"path":"G:/goproject/go/logAgent/demo/web.log","topic":"web_log"},{"path":"G:/goproject/go/logAgent/demo/randy.log","topic":"randy_log"}]
-	// 针对每一个日志收集项创建一个taskObj
-
-	// 打开文件读取日志
-	for _, conf := range allEtcdConf{
-
-		tt := newTailTask(conf.Path, conf.Topic) // 创建一个日志收集任务
-		err := tt.Init() // 去打开日志文件准备去读
-		if err != nil{
-			logrus.Errorf("create tailObj for path:%s, failed, err: %v", conf.Path, err)
-		}
-		logrus.Infof("create atail task for path: %s success", conf.Path)
-		// 启一个后台的goroutine去收集日志
-		go tt.run()
-	}
-
-	return
 }
--- core/tailfile/tailfielMgr.go
+++ core/tailfile/tailfielMgr.go
@@ -1,5 +1,106 @@
 package tailfile
+
+import (
+	"github.com/sirupsen/logrus"
+	"logAgent/common"
+)
+
 /*
 @author RandySun
 @create 2021-09-12-23:46
 */
+
+// tailTask 的管理者
+type tailTaskMgr struct {
+	tailTaskMap      map[string]*tailTask       // 收集日志对象 所有的tailTask任务
+	collectEntryList []common.CollectEntry      // 启动时候的日志文件配置,所有配置项
+	confChan         chan []common.CollectEntry // etcd日志配置发生了变化的监控, 等待新配置项
+
+}
+
+var (
+	ttMgr *tailTaskMgr
+)
+
+// 初始化tailTask,为每一个日志文件构造一个单独的tailTask
+func Init(allEtcdConf []common.CollectEntry) (err error) {
+	// allEtcdConf里面的内容: [{"path":"G:/goproject/go/logAgent/demo/web.log","topic":"web_log"},{"path":"G:/goproject/go/logAgent/demo/randy.log","topic":"randy_log"}]
+	ttMgr = &tailTaskMgr{
+		tailTaskMap:      make(map[string]*tailTask, 20),
+		collectEntryList: allEtcdConf,
+		confChan:         make(chan []common.CollectEntry), //  // 初始化新配置的管道 做一个阻塞的channel
+	}
+	// 打开文件读取日志  针对每一个日志收集项创建一个taskObj
+	for _, conf := range allEtcdConf {
+
+		tt := newTailTask(conf.Path, conf.Topic) // 创建一个日志收集任务
+		err := tt.Init()                         // 去打开日志文件准备去读
+		if err != nil {
+			logrus.Errorf("create tailObj for path:%s, failed, err: %v", conf.Path, err)
+		}
+		logrus.Infof("create atail task for path: %s success", conf.Path)
+		ttMgr.tailTaskMap[tt.path] = tt // 把创建这个tailTask任务登记在册,方便后续管理
+		// 启一个后台的goroutine去收集日志
+		go tt.run()
+	}
+
+	go ttMgr.watch() // 在后台等待新的配置过来
+	return
+}
+
+func (t *tailTaskMgr) watch() {
+	for {
+		// 派一个小弟等待新配置来
+		newConf := <-t.confChan // 取到值说明新的配置来了
+		// 新配置来了后应该管理一下我之前启动的那些tailTask
+
+		logrus.Infof("get new conf from etcd , conf: %v, start manage tailTask....", newConf)
+		for _, conf := range newConf {
+			// 1. 原来已经存在的任务就不用动
+			if t.isTailTaskExist(conf) {
+				continue
+			}
+			// 2. 原来没有的我要创建一个新的tailTask任务
+			tt := newTailTask(conf.Path, conf.Topic) // 创建一个日志收集任务
+			err := tt.Init()                         // 去打开日志文件准备去读
+			if err != nil {
+				logrus.Errorf("new create tailObj for path:%s, failed, err: %v", conf.Path, err)
+				continue
+			}
+			logrus.Infof("new create atail task for path: %s success", conf.Path)
+			t.tailTaskMap[tt.path] = tt // 把创建这个tailTask任务登记在册,方便后续管理
+			// 启一个后台的goroutine去收集日志
+			go tt.run()
+
+		}
+		// 3. 原来有的现在没有的要tailTask任务停掉
+		// 3.1 找出tailTaskMap中存在,但是newConf不存在的那些tailTask,把他们都停掉
+		for key, task := range t.tailTaskMap {
+			var flage bool
+			for _, conf := range newConf {
+				if key == conf.Path {
+					flage = true
+				}
+			}
+			if !flage {
+				// 这个tailTask要停掉
+				logrus.Infof("the task collect path: %s need to stop", task.path)
+				delete(t.tailTaskMap, key) // 从管理类中删除掉
+				task.cancel()
+			}
+		}
+	}
+
+}
+
+// 判断tailTaskMap是否存在该项收集,新的配置
+func (t *tailTaskMgr) isTailTaskExist(conf common.CollectEntry) bool {
+	_, ok := t.tailTaskMap[conf.Path]
+	return ok
+
+}
+
+// 将监听的配置发生变化保存到chan中
+func SendNewConf(newConf []common.CollectEntry) {
+	ttMgr.confChan <- newConf
+}

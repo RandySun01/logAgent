4901096e7bfb21779d03caa7e60da85536526f42
RandySun01 <2214644978@qq.com>
2021-09-23 22:57:46 +0800

    操作influxdb1.7版本

--- demo/09influxdbversion2.0.go
+++ demo/09influxdbversion2.0.go
@@ -77,24 +77,3 @@ func main() {
 	writesPoints(client, org, bucket)
 	queryDB(client, "RandySun")
 }
-
-//func main() {
-//	conn := connInflux()
-//	fmt.Println(conn)
-//
-//	// insert
-//	writesPoints(conn)
-//
-//	// 获取10条数据并展示
-//	qs := fmt.Sprintf("SELECT * FROM %s LIMIT %d", "cpu_usage", 10)
-//	res, err := queryDB(conn, qs)
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//
-//	for _, row := range res[0].Series[0].Values {
-//		for j, value := range row {
-//			log.Printf("j:%d value:%v\n", j, value)
-//		}
-//	}
-//}

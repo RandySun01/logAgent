74f9147d295e3bc76c24c0203651b421f73cfd7b
RandySun01 <2214644978@qq.com>
2021-09-27 22:42:42 +0800

    es操作

---
+++ demo/12es.go
@@ -0,0 +1,41 @@
+package main
+
+/*
+@author RandySun
+@create 2021-09-27-8:22
+*/
+import (
+	"context"
+	"fmt"
+
+	"github.com/olivere/elastic/v7"
+)
+
+// Elasticsearch demo
+
+type Person struct {
+	Name    string `json:"name"`
+	Age     int    `json:"age"`
+	Married bool   `json:"married"`
+}
+
+func main() {
+	client, err := elastic.NewClient(elastic.SetURL("http://127.0.0.1:9200"))
+	if err != nil {
+		// Handle error
+		panic(err)
+	}
+
+	fmt.Println("connect to es success")
+	// 创建一条数据
+	p1 := Person{Name: "rion", Age: 22, Married: false}
+	put1, err := client.Index().
+		Index("user").
+		BodyJson(p1).
+		Do(context.Background())
+	if err != nil {
+		// Handle error
+		panic(err)
+	}
+	fmt.Printf("Indexed user %s to index %s, type %s\n", put1.Id, put1.Index, put1.Type)
+}
